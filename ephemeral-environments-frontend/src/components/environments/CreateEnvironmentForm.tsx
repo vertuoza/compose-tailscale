d24a8f0695276a4cfdb99e22c0fb2519ec50f546
Maxime Bosquet <maxime.bosquet@vertuoza.com>
2025-05-16 12:13:10 +0200

    fix tailscale state volume

--- ephemeral-environments-frontend/src/components/environments/CreateEnvironmentForm.tsx
+++ ephemeral-environments-frontend/src/components/environments/CreateEnvironmentForm.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { createEnvironment, CreateEnvironmentRequest } from '../../services/api';
 import Button from '../common/Button';
